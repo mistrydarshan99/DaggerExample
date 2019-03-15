f660ce5183c2681f9a8b868e56c130068d2d1316
darshan.mistry <darshan.mistry@softwebsolutions.com>
2019-03-15 18:51:58 +0530

    call webservice using dagger, coroutine and retrofit.

--- app/src/main/java/com/darshan/daggerexample/feature/PostListActivity.kt
+++ app/src/main/java/com/darshan/daggerexample/feature/PostListActivity.kt
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import com.darshan.daggerexample.R
 import com.darshan.daggerexample.feature.viewModel.PostViewModel
 import com.darshan.daggerexample.feature.viewModel.inject
+import io.plaidapp.core.util.delegates.contentView
 import javax.inject.Inject
 
 class PostListActivity : AppCompatActivity() {
 
   @Inject
   internal lateinit var viewModel: PostViewModel
+
+  private val binding by contentView<PostListActivity, com.darshan.daggerexample.databinding.ActivityMainBinding>(
+    R.layout.activity_main
+  )
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
--- app/src/main/java/com/darshan/daggerexample/module/PostDataModule.kt
+++ app/src/main/java/com/darshan/daggerexample/module/PostDataModule.kt
@@ -4,12 +4,14 @@ import com.darshan.daggerexample.api.WebServices
 import com.darshan.daggerexample.base.CoroutinesDispatcherProvider
 import com.darshan.daggerexample.feature.viewModel.PostRemoteDataSource
 import com.darshan.daggerexample.feature.viewModel.PostRepository
+import com.google.gson.Gson
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import dagger.Lazy
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 @Module(includes = [CoreDataModule::class])
 class PostDataModule {
@@ -23,12 +25,14 @@ class PostDataModule {
   @Provides
   fun provideApiService(
     client: Lazy<OkHttpClient>,
-    callAdapterFactory: CoroutineCallAdapterFactory
+    callAdapterFactory: CoroutineCallAdapterFactory,
+    gson: Gson
   ): WebServices =
     Retrofit.Builder()
       .baseUrl(WebServices.ENDPOINT)
       .callFactory { client.get().newCall(it) }
       .addCallAdapterFactory(callAdapterFactory)
+      .addConverterFactory(GsonConverterFactory.create(gson))
       .build()
       .create(WebServices::class.java)
 }
